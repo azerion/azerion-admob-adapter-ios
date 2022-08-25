9e69ef9b988996551ad102c4824616d4a7976785
Torongo <torongo.azad@vivacomsolutions.com>
2022-08-25 18:09:58 +0600

    AdMob adapter for azerion ads

---
+++ AzerionAdsAbmobAdapter.xcframework/ios-arm64_armv7/AzerionAdsAbmobAdapter.framework/Headers/AZAAdMobInitializer.h
@@ -0,0 +1,18 @@
+//
+//  AZAAdMobInitializer.h
+//  AzerionAds
+//
+//  Created by Nagib Bin Azad on 6/9/21.
+//  Copyright © 2021 Azerion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AzerionAdsMediationCore/AzerionAdsMediationCore.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AZAAdMobInitializer : NSObject <AZAAdNetworkInitializer>
+
+@end
+
+NS_ASSUME_NONNULL_END
